f17985a481494fb21a23c9053c174c3b64170750
Joshua Yuan <joshua43210@gmail.com>
2016-12-29 23:17:08 -0800

    flexbox fixes

--- scripts.js
+++ scripts.js
@@ -1,20 +1,20 @@
 $(document).ready(function(){
     $(document).click(function(event) {
-        if($(event.target).is('#proj1')){
-            console.log("hide proj1, show proj2");
+        if($(event.target).is('#proj1') || $(event.target).is('#proj1 p') ){
+            // console.log("hide proj1, show proj2");
             $('#proj2').show();
-        } else if ($(event.target).is('#about1')){
-            console.log("hide about1, show about2");
+        } else if ($(event.target).is('#about1') || $(event.target).is('#about1 p')){
+            // console.log("hide about1, show about2");
             $('#about2').show();
         }
         else if(!$(event.target).closest('#proj2').length && !$(event.target).closest('#about2').length) {
-            console.log("clicked outside proj2 and outside about2");
+            // console.log("clicked outside proj2 and outside about2");
             if($('#proj2').is(":visible")) {
-                console.log("hiding proj2");
+                // console.log("hiding proj2");
                 $('#proj2').hide();
             }
             if($('#about2').is(":visible")) {
-                console.log("hiding about2");
+                // console.log("hiding about2");
                 $('#about2').hide();
             }
         }       
